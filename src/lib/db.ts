af6b5f76474ed35c1e67762404f151ce1bed2363
leconstantin <lcon69184@gmail.com>
2025-01-22 20:54:51 +0200

    added accelerate not worked

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,14 @@
 // lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 import { withOptimize } from "@prisma/extension-optimize";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient().$extends(
-    withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY })
-  );
+  new PrismaClient()
+    .$extends(withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY }))
+    .$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
